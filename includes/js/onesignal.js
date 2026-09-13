bffc111b8805961ee78c4dad7fcf4753cf8299f7
@RRdeveloper-01 <rohitmaurya89579406@gmail.com>
2026-09-13 08:05:44 +0530

    update

--- includes/js/onesignal.js
+++ includes/js/onesignal.js
@@ -1,10 +1,6 @@
 window.OneSignalDeferred = window.OneSignalDeferred || [];
-OneSignalDeferred.push(async function(OneSignal) {
-  await OneSignal.init({
-    appId: "18fa4ad8-4d7f-46d3-b4d8-6d509aa9c425",
-    notifyButton: {
-      enable: true,
-    },
-    allowLocalhostAsSecureOrigin: true
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "18fa4ad8-4d7f-46d3-b4d8-6d509aa9c425",
+    });
   });
-});
